d92194fe39d217c56740383a58b271ef27f190e0
yashgopani41 <gopaniyash568@gmail.com>
2024-02-12 15:41:05 +0530

    done

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+"use server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verifyemail";
+
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup", "/verifyemail"],
+};
